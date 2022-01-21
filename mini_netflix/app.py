57f0ed29acdd2b34216663be1c396879f092160c
POTUSAITEJA <saitejareddypotu@gmail.com>
2022-01-21 12:14:54 +0100

    sai work

---
+++ mini_netflix/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/imageid")
+def imageid():
+    return render_template("imageid.html")
+
+@app.route("/webscraping")
+def webscraping():
+    return render_template("webscraping.html")
+
+if __name__ == "__main__":
+    app.run(debug=True) 
